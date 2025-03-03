1433a957fa3b3953df17859224bd252193e40af1
baekchangjoon <changjoon.baek@icloud.com>
2025-03-03 23:11:12 +0900

    Bugfix for SocketTransformer

--- src/main/java/com/example/agent/SocketInterceptorAgent.java
+++ src/main/java/com/example/agent/SocketInterceptorAgent.java
@@ -1,8 +1,13 @@
 package com.example.agent;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.net.URISyntaxException;
+import java.security.CodeSource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.jar.JarFile;
 
 public class SocketInterceptorAgent {
     public static void premain(String agentArgs, Instrumentation inst) {
@@ -10,6 +15,14 @@ public class SocketInterceptorAgent {
 
         // 에이전트 인자 파싱 (logFile=/path/to/log.txt 형식 가정)
         Map<String, String> config = parseAgentArgs(agentArgs);
+
+        // JAR 를 bootstrap classloader search path에 추가
+        try {
+            inst.appendToBootstrapClassLoaderSearch(new JarFile("/Users/changjoonbaek/github_socketInterceptor/SocketInterceptorAgent/target/SocketInterceptorAgent-1.0-SNAPSHOT.jar"));
+            // 이제 Bootstrap ClassLoader에서도 InterceptedXXX 클래스를 볼 수 있음
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Transformer 등록
         inst.addTransformer(new SocketTransformer(config), true);
